5414d5f9ce3015074a394d118b213e95d752c652
Hady-Hassan <90941057+Hady-Hassan@users.noreply.github.com>
2022-05-25 00:22:47 +0200

    Enemy AI , Spawner

--- My project/Assets/Scripts/AiPathFinding.cs
+++ My project/Assets/Scripts/AiPathFinding.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class AiPathFinding : MonoBehaviour
+public class chasePlayer : MonoBehaviour
 {
+    NavMeshAgent agent;
+    GameObject[] targets;
     // Start is called before the first frame update
     void Start()
     {
+        agent = this.GetComponent<NavMeshAgent>();
+        targets = GameObject.FindGameObjectsWithTag("Npc");
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        targets = GameObject.FindGameObjectsWithTag("Npc");
+        foreach (GameObject target in targets)
+        {
+            agent.SetDestination(target.transform.position);
+        }
     }
 }
--- My project/Assets/Scripts/AnimalSpawner.cs
+++ My project/Assets/Scripts/AnimalSpawner.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class AnimalSpawner : MonoBehaviour
 {
+    public GameObject[] NPCs;
+    public GameObject[] loaded;
+    public float timer = 20;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawn();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+
+        loaded = GameObject.FindGameObjectsWithTag("Npc");
+
+        if (timer <= 0)
+        {
+            timer = 20;
+            spawn();
+            
+        }
     }
+
+    void spawn()
+    {
+        for(int i =0;  i < 5;++i)
+        {
+            int z = Random.Range(-18, 75);
+            int x = Random.Range(-9, 85);
+            int y = Random.Range(0, 3);
+            Vector3 pos = new Vector3(x, y, z);
+            int index = Random.Range(0, 4);
+            GameObject npc = NPCs[index];
+            npc.transform.position = pos;
+            Instantiate(npc,this.transform);
+        }
+    }
+
 }
