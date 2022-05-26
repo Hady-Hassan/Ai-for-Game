1b01be3c94a735d1f1f94b7f929ad725c1a5027e
Mk662000 <mk840166@gmail.com>
2022-05-26 03:23:26 +0200

    main menu

--- My project/Assets/gameover.cs
+++ My project/Assets/gameover.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dumb : MonoBehaviour
+public class gameover : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
+        
     }
 
     // Update is called once per frame
